37718172e5507398d2cb42358fb7e61527e3357e
Luna222 <thao@ordinaryfolk.health>
2024-03-08 15:59:03 +0700

    up

--- models/User.js
+++ models/User.js
@@ -6,6 +6,8 @@ class User {
   //Private fields on Instances
   #uId = `U${(Date.now() + '').slice(-10)}`;
   #NEWS_API_KEY = 'ba6f52a1d24942a2ab3f29d0628ee7e5';
+  #prevCheck = false;
+  #nextCheck = false;
 
   constructor(firstName, lastName, userName, password) {
     this.firstName = firstName;
@@ -14,11 +16,25 @@ class User {
     this.password = password;
 
     this._setWelcome();
+
+    //Attach event handlers
+    btnPrev.addEventListener('click', this._isPrev.bind(this));
+    btnNext.addEventListener('click', this._isNext.bind(this));
   }
 
   //[Private Methods]
   _setWelcome() {
     this.welcome = `Welcome ${this.firstName}! 🌻`;
+  }
+
+  _isPrev() {
+    this.#prevCheck = true;
+    this.#nextCheck = false;
+  }
+
+  _isNext() {
+    this.#nextCheck = true;
+    this.#prevCheck = false;
   }
 
   _renderError(errMsg) {
@@ -92,13 +108,19 @@ class User {
         pageSize = 5;
 
       let page = 0;
+
       let dataNews, pagination, lastPage;
 
       return async function () {
         try {
-          page++;
+          if (this.#prevCheck && page > 1) {
+            page--;
+          } else {
+            page++;
+          }
 
-          if (page === 1) {
+          //load page the first time
+          if (page === 1 && !this.#prevCheck) {
             dataNews = await this._getReqData.call(
               this,
               `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=${category}&pageSize=${pageSize}&page=${page}&apiKey=${
@@ -124,10 +146,16 @@ class User {
               pagination.append(updatedDataNews);
             }
             pagination.traverseForward();
-          } else {
-            this._renderNews(pagination.traverseForward());
-            this._updatePagination(page, lastPage);
           }
+
+          if (this.#nextCheck) {
+            this._renderNews(pagination.traverseForward());
+          }
+
+          if (this.#prevCheck) {
+            this._renderNews(pagination.traverseBackward());
+          }
+          this._updatePagination(page, lastPage);
         } catch (err) {
           console.error('Error occurred while fetching data 💥:', err.message);
           //render error msg for User
@@ -137,42 +165,6 @@ class User {
           throw err;
         }
       };
-    }
-  }
-
-  async getPrevNews(page) {
-    //if User logged in successfully
-    if (isLoggedIn) {
-      try {
-        const countryCode = 'us',
-          category = 'general',
-          pageSize = 5;
-
-        if (page > 1) page--;
-        console.log(page);
-
-        const pagination = new Pagination();
-        for (let i = page; i >= 1; i--) {
-          const updatedDataNews = await this._getReqData.call(
-            this,
-            `https://newsapi.org/v2/top-headlines?country=${countryCode}&category=${category}&pageSize=${pageSize}&page=${i}&apiKey=${
-              this.#NEWS_API_KEY
-            }`
-          );
-          pagination.append(updatedDataNews);
-        }
-        // pagination.traverseBackward();
-
-        this._renderNews(pagination.traverseBackward());
-        this._updatePagination(page);
-      } catch (err) {
-        console.error('Error occurred while fetching data 💥:', err.message);
-        //render error msg for User
-        this._renderError(
-          `Something went wrong 💥: ${err.message}. Please try again!`
-        );
-        throw err;
-      }
     }
   }
 }
