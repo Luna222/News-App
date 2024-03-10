f62ac6fb89e23dc4cbca4caf710d6caca99c791c
Luna222 <thao@ordinaryfolk.health>
2024-03-10 22:02:15 +0700

    up

--- scripts/news.js
+++ scripts/news.js
@@ -29,7 +29,7 @@ initNews();
  * Handle Events
  ******************************************************************************/
 /*
-to navigate to the next page while rendering the corresponding data, use Closure behavior in JS
+to navigate back n forth through the pages while rendering the corresponding data, I will use Closure behavior in JS
 */
 /**
  *
