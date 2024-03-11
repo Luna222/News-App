ac085f822b1a66bcb8a4dfd9774db95a1ca87f5b
Luna222 <thao@ordinaryfolk.health>
2024-03-11 22:55:04 +0700

    up

--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,6 @@
 ///////////////////////////////////////
 // TASK
 class Task {
-  //Public fields on Instances
-  // taskId = `T${(Date.now() + '').slice(-10)}`;
-
   constructor(task, owner, isDone) {
     this.task = task;
     this.owner = owner;
